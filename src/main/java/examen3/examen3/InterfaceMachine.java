c87e217848d90acb64378243925cf6f02402b19f
Ramon Rodriguez <ramon.rodriguez.mt.@correo.usa.edu.co>
2021-11-11 12:57:41 -0500

    first commit

---
+++ src/main/java/examen3/examen3/InterfaceMachine.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package examen3.examen3;
+
+/**
+ *
+ * @author ramon rodriquez
+ */
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author ramon rodriquez
+ */
+public interface InterfaceMachine extends CrudRepository<Machine,Integer>{
+}
